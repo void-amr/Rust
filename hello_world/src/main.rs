73bdb97b48f3eddd6f65d782086472d379d27eab
void <aryanranjane9@gmail.com>
2024-06-27 15:04:22 +0530

    trying this sublime integreation of github

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -2,12 +2,10 @@
  * @Author -> AryanRanjane(ranjanearyan14@gmail.com)
  * @Date -> 27/6/24
  */
+fn main() {    
+  hello_world(); 
+}
 
-
- fn main() {    
-    hello_world(); 
- }
-
- fn hello_world() { 
-    println!("Hello,World!.");
- }
+fn hello_world() { 
+  println!("Hello,World!.");
+}
